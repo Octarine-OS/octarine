fe00de786d7c01d4863c8239de0adc6613327566
Devin Nakamura <devin122@gmail.com>
2019-06-30 21:10:01 -0400

    Add strcat and strcpy

--- klib/include/string.h
+++ klib/include/string.h
@@ -7,13 +7,13 @@
 void *memcpy(void* dest, const void* src, size_t n);
 //void *memmove(void* dest, const void* src, size_t n);
 void *memset(void* str, int c, size_t n);
-//char *strcat(char *dest, const char *src);
+char *strcat(char *dest, const char *src);
 //char *strncat(char *dest, const char* src, size_t n);
 //char *strchr(const char* str, int c);
 //int strcmp(const char* str1, const char* str2);
 //int strncmp(const char* str1, const char *str2, size_t n);
 //int strcoll(const char* str1, const char *str2);
-//char* strcpy(char *dest, const char *src);
+char* strcpy(char *dest, const char *src);
 //char* strncpy(char *dest, const char* src, size_t n);
 //size_t strcspn(const char *str1, const char *str2);
 //char *strerror(int errnum);
