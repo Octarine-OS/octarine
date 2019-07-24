59a65132d265b8f57be636e67069cfea17b1ea90
Devin Nakamura <devin122@gmail.com>
2019-07-23 23:19:54 -0400

    redefine PIC data ports to be relative the command ports

--- kernel/arch/i386/PIC.cpp
+++ kernel/arch/i386/PIC.cpp
@@ -31,10 +31,10 @@
 
 namespace {
 constexpr uint16_t PIC1_COMMAND = 0x20;
-constexpr uint16_t PIC1_DATA = 0x21;
+constexpr uint16_t PIC1_DATA = PIC1_COMMAND + 1;
 
 constexpr uint16_t PIC2_COMMAND = 0xA0;
-constexpr uint16_t PIC2_DATA = 0xA1;
+constexpr uint16_t PIC2_DATA = PIC2_COMMAND + 1;
 
 constexpr uint8_t PIC_EOI = 0x20;
 
