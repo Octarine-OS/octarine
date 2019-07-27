50cea2862cc82207d5d4c82dcf7ee758415b6928
Devin Nakamura <devin122@gmail.com>
2019-07-27 17:39:54 -0400

    reformat i38 Context.hpp

--- arch/i386/include/machine/Context.hpp
+++ arch/i386/include/machine/Context.hpp
@@ -3,32 +3,30 @@
 
 #include <stdint.h>
 
-
-//note this matches layout of "PUSHAD" func
-//TODO: save segment register state when that is an issue
+// note this matches layout of "PUSHAD" func
+// TODO: save segment register state when that is an issue
 namespace i386 {
 struct Context {
-  uint32_t edi;
-  uint32_t esi;
-  uint32_t ebp;
-  uint32_t esp;
-  uint32_t ebx;
-  uint32_t edx;
-  uint32_t ecx;
-  uint32_t eax;
-  
-  uint32_t intNum;
-  uint32_t errCode;
-  
-  uint32_t eip;
-  uint32_t cs;
-  uint32_t eflags;
-};
-}
+	uint32_t edi;
+	uint32_t esi;
+	uint32_t ebp;
+	uint32_t esp;
+	uint32_t ebx;
+	uint32_t edx;
+	uint32_t ecx;
+	uint32_t eax;
 
-namespace arch{
+	uint32_t intNum;
+	uint32_t errCode;
+
+	uint32_t eip;
+	uint32_t cs;
+	uint32_t eflags;
+};
+} // namespace i386
+
+namespace arch {
 using Context = i386::Context;
 }
-
 
 #endif
